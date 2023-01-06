73c9a8e78ee5ab5637cf0a76da4c1389b43c3c0a
mateuseap <meap@cin.ufpe.br>
2023-01-06 06:30:28 -0300

    feat: 'ValidationPipe' added to the project

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppDataSource } from './app.datasource';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/dashboard/api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Dashboard Back')
